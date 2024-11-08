b1d691363220fe298e5d7d12f816a6945f7d8b6e
ChaDongmin <cdm1263@gmail.com>
2024-11-08 14:03:38 +0900

    feat: 레이아웃 및 정렬 로직 구현
    - grid로 레이아웃 구성
    
    - 오름, 내림차순 아이콘 추가 및 핸들러 적용
    
    - 데이터를 재정렬하는 sortData 함수 구성
    
    - 한번에 보여줄 데이터 수 만큼 전체 데이터를 잘라낸 slicedData를 이용해 데이터 표시

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { ArrowDown, ArrowUp } from "lucide-react";
+
 import "./App.css";
 
 interface UserData {
@@ -14,9 +16,23 @@ interface UserData {
   };
 }
 
+type Sort =
+  | "scoreUp"
+  | "scoreDown"
+  | "winsUp"
+  | "winsDown"
+  | "lossesUp"
+  | "lossesDown"
+  | "rateUp"
+  | "rateDown";
+
 function App() {
   const [data, setData] = useState<UserData[]>([]);
+  const [slicedData, setSlicedData] = useState<UserData[]>([]);
   const [loading, setLoading] = useState(false);
+  const [listViews, setListViews] = useState(10);
+  const [page, setPage] = useState(1);
+  const [sort, setSort] = useState<Sort>("scoreDown");
 
   const fetchUserData = async () => {
     setLoading(true);
@@ -33,7 +49,93 @@ function App() {
     fetchUserData();
   }, []);
 
-  return <></>;
+  const calculateRate = (wins: number, losses: number) => {
+    return Math.round((wins / (wins + losses)) * 1000) / 10;
+  };
+
+  const sortData = (data: UserData[]) => {
+    return [...data].sort((a, b) => {
+      switch (sort) {
+        case "scoreUp":
+          return a.score - b.score;
+        case "scoreDown":
+          return b.score - a.score;
+        case "winsUp":
+          return a.wins - b.wins;
+        case "winsDown":
+          return b.wins - a.wins;
+        case "lossesUp":
+          return a.losses - b.losses;
+        case "lossesDown":
+          return b.losses - a.losses;
+        case "rateUp":
+          return (
+            calculateRate(a.wins, a.losses) - calculateRate(b.wins, b.losses)
+          );
+        case "rateDown":
+          return (
+            calculateRate(b.wins, b.losses) - calculateRate(a.wins, a.losses)
+          );
+        default:
+          return 0;
+      }
+    });
+  };
+
+  useEffect(() => {
+    const sorted = sortData(data);
+    setSlicedData(sorted.slice(0, page * listViews));
+  }, [data, sort, page]);
+
+  return (
+    <div className="table-wrapper">
+      <div className="grid-container table-header">
+        <div className="grid-header">Player Name</div>
+        <div className="grid-header">Player Id</div>
+        <div className="grid-header sort">
+          <ArrowUp onClick={() => setSort("scoreUp")} />
+          <div>Score</div>
+          <ArrowDown onClick={() => setSort("scoreDown")} />
+        </div>
+        <div className="grid-header sort">
+          <ArrowUp onClick={() => setSort("winsUp")} />
+          <div>Wins</div>
+          <ArrowDown onClick={() => setSort("winsDown")} />
+        </div>
+        <div className="grid-header sort">
+          <ArrowUp onClick={() => setSort("lossesUp")} />
+          <div>Losses</div>
+          <ArrowDown onClick={() => setSort("lossesDown")} />
+        </div>
+        <div className="grid-header sort">
+          <ArrowUp onClick={() => setSort("rateUp")} />
+          <div>Win Rate</div>
+          <ArrowDown onClick={() => setSort("rateDown")} />
+        </div>
+        <div className="grid-header">Guild</div>
+      </div>
+
+      <div>
+        {slicedData &&
+          slicedData.map((content) => (
+            <div className="grid-container grid-row" key={content.player.id}>
+              <div className="grid-item">{content.player.name}</div>
+              <div className="grid-item">{content.player.id}</div>
+              <div className="grid-item">{content.score}</div>
+              <div className="grid-item">{content.wins}</div>
+              <div className="grid-item">{content.losses}</div>
+              <div className="grid-item">
+                {calculateRate(content.wins, content.losses)}%
+              </div>
+              <div className="grid-item">
+                {content.player.guild?.name ?? "-"}
+              </div>
+            </div>
+          ))}
+      </div>
+      {loading && <p>loading...</p>}
+    </div>
+  );
 }
 
 export default App;
