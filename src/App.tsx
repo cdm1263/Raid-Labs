b71bd0f9faed1a2f1b6d784d413ce3cb0daad9c1
ChaDongmin <cdm1263@gmail.com>
2024-11-08 10:31:15 +0900

    feat: 더미데이터 호출 로직 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+interface UserData {
+  score: number;
+  wins: number;
+  losses: number;
+  player: {
+    id: number;
+    name: string;
+    guild?: {
+      name: string;
+    };
+  };
+}
+
 function App() {
+  const [data, setData] = useState<UserData[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchUserData = async () => {
+    setLoading(true);
+    const response = await fetch(
+      "https://gateway.pinata.cloud/ipfs/bafkreia2tigtk5kv5x6mptrscob7rwyvooyzte2j7luimkfssvm3m2zf54"
+    );
+
+    const data = await response.json();
+    setData(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
   return <></>;
 }
 
